c3ad1813d582a4506d40fc1008f63de6121f18ee
Steve Blythe <blythesr@gmail.com>
2022-02-16 19:37:10 +0000

    rw-combine-ch3-transformingoperators

--- 03-transforming-operators/projects/Starter.playground/Contents.swift
+++ 03-transforming-operators/projects/Starter.playground/Contents.swift
@@ -3,36 +3,141 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+// collect()
+// The collect operator provides a convenient way to transform a stream of individual values from a publisher into a single array.
+example(of: "collect") {
+    ["A", "B", "C", "D", "E"].publisher
+        .collect(2)
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
 
-/// Copyright (c) 2021 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+// map()
+// This works just like Swift’s standard map, except that it operates on values emitted from a publisher.
+example(of: "map") {
+    // Create a number formatter to spell out each number.
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .spellOut
+    
+    // Create a publisher of integers.
+    [123, 4, 56].publisher
+        .map {
+            formatter.string(for: NSNumber(integerLiteral: $0)) ?? ""
+        }
+        .sink { print($0) }
+        .store(in: &subscriptions)
+}
+
+// mapping key paths
+//
+example(of: "mapping key paths") {
+    // Create a publisher of Coordinates that will never emit an error.
+    let publisher = PassthroughSubject<Coordinate, Never>()
+    
+    // Begin a subscription to the publisher
+    publisher
+        // Map into the x and y properties of Coordinate using their key paths.
+        .map(\.x, \.y)
+        // Print a statement that indicates the quadrant of the provide x and y values.
+        .sink { (x, y) in
+            print("The coordinate at (\(x), \(y)) is in quadrant", quadrantOf(x: x, y: y))
+        }
+        .store(in: &subscriptions)
+    
+    publisher.send(Coordinate(x: 10, y: -8))
+    publisher.send(Coordinate(x: 0, y: 5))
+}
+
+// trymap()
+// Several operators, including map, have a counterpart with a try prefix that takes a throwing closure. If you throw an error, the operator will emit that error downstream.
+example(of: "tryMap") {
+    // Create a publisher of a string representing a directory name that does not exist.
+    Just("Directory name that does not exist")
+        // Use tryMap to attempt to get the contents of that nonexistent directory.
+        .tryMap { try FileManager.default.contentsOfDirectory(atPath: $0) }
+        // Receive and print out any values or completion events.
+        .sink {
+            print($0)
+        } receiveValue: {
+            print($0)
+        }
+        .store(in: &subscriptions)
+}
+
+// flatMap(maxPublishers:_:)
+// The flatMap operator flattens multiple upstream publishers into a single downstream publisher — or more specifically, flatten the emissions from those publishers.
+// A common use case for flatMap in Combine is when you want to pass elements emitted by one publisher to a method that itself returns a publisher, and ultimately subscribe to the elements emitted by that second publisher.
+example(of: "flatMap") {
+    // Define a function that takes an array of integers, each representing an ASCII code, and returns a type-erased publisher of strings that never emits errors.
+    func decode(_ codes: [Int]) -> AnyPublisher<String, Never> {
+        // Create a Just publisher that converts the character code into a string if it’s within the range of 0.255, which includes standard and extended printable ASCII characters.
+        Just(
+          codes
+            .compactMap { code in
+              guard (32...255).contains(code) else { return nil }
+              return String(UnicodeScalar(code) ?? " ")
+            }
+            // Join the strings together.
+            .joined()
+        )
+        // Type erase the publisher to match the return type for the fuction.
+        .eraseToAnyPublisher()
+    }
+    
+    // Create a secret message as an array of ASCII character codes, convert it to a publisher, and collect its emitted elements into a single array.
+    [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+        .publisher
+        .collect()
+        // Use flatMap to pass the array element to your decoder function.
+        .flatMap(decode)
+        // Subscribe to the elements emitted by the pubisher returned by decode(_:) and print out the values.
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+// replaceNil(with:)
+// replaceNil will receive optional values and replace nils with the value you specify
+example(of: "replaceNil") {
+    // Create a publisher from an array of optional strings.
+    ["A", nil, "C"].publisher
+        .eraseToAnyPublisher()
+        // Use replaceNil(with:) to replace nil values received from the upstream publisher with a new non-nil value.
+        .replaceNil(with: "*")
+        .sink { print($0) }
+        .store(in: &subscriptions)
+}
+
+// replaceEmpty(with:)
+// You can use the replaceEmpty(with:) operator to replace — or really, insert — a value if a publisher completes without emitting a value.
+example(of: "replaceEmpty") {
+    // Create an empty publisher that immediately emits a completion event.
+    let empty = Empty<Int, Never>()
+    
+    // Subscribe to it, and print received events.
+    empty
+        .replaceEmpty(with: 5)
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+// scan(_:_:)
+// scan provides the current value emitted by an upstream publisher to a closure, along with the last value returned by that closure.
+example(of: "scan") {
+    // Create a computed property that generates a random integer between -10 and 10.
+    var dailyGainLoss: Int { .random(in: -10...10) }
+    
+    // Use that generator to create a publisher from an array of random integers representing fictitious daily stock price changes for a month.
+    let august2019 = (0..<22).map { _ in
+        dailyGainLoss
+    }.publisher
+    
+    // Use scan with a starting value of 50, and then add each daily change to the running stock price. The use of max keeps the price non-negative — thankfully stock prices can’t fall below zero!
+    august2019
+        .scan(50) { latest, current in
+            max(0, latest + current)
+        }
+        .sink(receiveValue: { _ in })
+        .store(in: &subscriptions)
+}
